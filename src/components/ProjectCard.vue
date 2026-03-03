c484787208d4efe08e3908063c967247d19e87e3
pjbatz16 <78991855+pjbatz16@users.noreply.github.com>
2026-03-03 21:19:32 +0800

    Add files via upload

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,27 @@
+<template>
+
+	<div class="card col-md-3">
+		<!-- directive to bind photo to the src attribute -->
+	    <img :src="project.image" class="card-img-top card-img" alt="...">
+	    <div class="card-body">
+	    <h5 class="card-title">{{project.title}}</h5>
+	    <p class="card-text">
+	       {{project.description}}
+	    </p>
+	    
+	    </div>
+	</div>
+
+
+</template>
+
+
+<script setup>
+
+	import {defineProps} from "vue";
+
+	defineProps({
+		project: Object
+	})
+	
+</script>
